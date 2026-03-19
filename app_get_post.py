21732e18ce618cb549db9ebde7c2a8e5165c13c4
IsabellaZamai <bellazamai10@gmail.com>
2026-03-19 11:05:40 -0300

    liçao do comit part2

--- app_get_post.py
+++ app_get_post.py
@@ -1,9 +1,10 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
-    class Servidor(BaseHTTPRequestHandler):
+class Servidor(BaseHTTPRequestHandler):
  
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
         self.wfile.write(b"Servidor funcionando")
- HTTPServer(("0.0.0.0", 8000), Servidor).serve_forever()
+HTTPServer(("0.0.0.0", 8000), Servidor).serve_forever()
+        #mudança 
