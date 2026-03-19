631408584eb856f69033eb083413e61b18a34eb5
IsabellaZamai <bellazamai10@gmail.com>
2026-03-19 10:45:00 -0300

    Lição do Commit

---
+++ app_get_post.py
@@ -0,0 +1,9 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+ class Servidor(BaseHTTPRequestHandler):
+ 
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+         self.wfile.write(b"Servidor funcionando")
+ HTTPServer(("0.0.0.0", 8000), Servidor).serve_forever()
